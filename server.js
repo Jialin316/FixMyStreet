779a6b96dfe7673bae9505aa08d51c037cce415d
WU Jialin <jialin.wu@student.uclouvain.be>
2024-11-14 17:23:42 +0100

    Formatage des fichiers et ajout de commentaire

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
+// Création de l'application
 const express = require("express");
 const app = express();
 
+// Set l'app pour supporter ejs
 app.set('view engine', 'ejs');
 
+// Page d'accuil
 app.get("/", function(req, res) {
     res.render("templates/acceuil.ejs");
 });
 
+// Page de connexion
 app.get("/connexion", function(req, res) {
     res.render("templates/connexion.ejs");
 });
 
+// Page d'ajout d'incident
 app.get("/signal", function(req, res) {
     res.render("templates/signalisation.ejs");
 });
 
 
-
 app.use(express.static('views'));
+// Lance le serveur sur le port 8080
 app.listen(8080);
